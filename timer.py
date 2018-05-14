7c8d725605f509ab47730f203634d64e6b98620f
knine3 <kawishqayyum@gmail.com>
2018-05-15 03:23:01 +0500

    feat: Add Settings for sessinon duration and exception handling.

--- timer.py
+++ timer.py
@@ -5,22 +5,42 @@ minutes = int(0)
 hours = int(0)
 TR = 'n'
 
-while (TR.lower()=="n"):
-    TR = input("Start Timer: (y/n)")
-
-while TR.lower() == 'y':
-    if seconds > 59:
-        seconds = 0
-        minutes += 1
-    
-    if minutes > 59:
-        mintues = 0
-        hours += 1
-    
-    print ("timer: {:02d}:{:02d}:{:05.2f}".format(hours, minutes, seconds), end = "\r")
-    time.sleep(0.01)
-    seconds += 0.01
-    
-    if minutes == 1:
+while(True):
+    try:
+        overflow = int(input("Set Work Session Duration (minutes): "))
         break
-    
+    except ValueError:
+        print("Invalid Value, try entering an integer.")
+
+while (TR.lower()!="y"):
+    TR = input("\nREADY TO GET SOME WORK DONE (y/n/e): ")
+    print ("Enter 'y' for Yes, 'n' for No or 'e' to exit.")
+    if (TR.lower() == 'e'):
+        exit()
+
+print ('Timer started', end='\t\t')
+print ("\t\tWork Session duration: {:02d}:{:02d}:{:05.2f}".format(hours, overflow, seconds), end = "\r" )
+
+try:
+    while TR.lower() == 'y':
+        if seconds > 59:
+            seconds = 0
+            minutes += 1
+        
+        if minutes > 59:
+            mintues = 0
+            hours += 1
+        
+        print ("\t\tWorking: {:02d}:{:02d}:{:05.2f}".format(hours, minutes, seconds), end = "\r")
+        time.sleep(0.01)
+        seconds += 0.01
+        
+        if minutes == overflow:
+            break
+
+except KeyboardInterrupt:
+    decision = input("\nFORFIET WORK SESSION(y/n): ")
+    if decision.lower() == 'y':
+        print ("Timer stopped at: {:02d}:{:02d}:{:05.2f}".format(hours, minutes, seconds), end = "\r")
+
+print ("\nWork Session Completed")    
