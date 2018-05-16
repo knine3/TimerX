cc39b9ea439cf30ebfc39d5254c71f92071f62c5
knine3 <kawishqayyum@gmail.com>
2018-05-16 07:02:22 +0500

    feat: Switch to Countdown

--- timer.py
+++ timer.py
@@ -5,16 +5,15 @@ minutes = int(0)
 hours = int(0)
 TR = 'n'
 
+#divider line.
 def div():
-    print()
-    print('  '+ 68*'-') #divider line.
-    print()
+    print('\n'+'  '+ 68*'-'+'\n')  
 
 div()
     
 while(True):
     try:
-        overflow = int(input("\t\tSet Work Session Duration (minutes): "))
+        minutes = int(input("\t\tSet Work Session Duration (minutes): "))
         break
     except ValueError:
         print("Invalid Value, try entering an integer.")
@@ -23,25 +22,21 @@ while (TR.lower()!="y"):
     TR = input("\t\t READY TO GET SOME WORK DONE (y/n): ")
     if (TR.lower() == 'n'):
         exit()
-print ()
-print ('\t\t\t    Timer started')
-print ("\t\t\tDuration: {:02d}:{:02d}:{:05.2f}".format(hours, overflow, seconds))
+
+print ('\n\t\t\t    Timer started')
 
 while(True):
+    
     try:
-        if seconds > 59:
-            seconds = 0
-            minutes += 1
-        
-        if minutes > 59:
-            mintues = 0
-            hours += 1
-        
         print ("\t\t\tWorking : {:02d}:{:02d}:{:05.2f}".format(hours, minutes, seconds), end = "\r")
         time.sleep(0.01)
-        seconds += 0.01
+        seconds -= 0.01
         
-        if minutes == overflow:
+        if seconds < 0:
+            seconds = 59.99
+            minutes -= 1
+        
+        if minutes < 0:
             break
     
     except KeyboardInterrupt:
@@ -54,5 +49,4 @@ while(True):
             continue
 
 print ("\t\t       Work Session Completed!")
-
 div()
